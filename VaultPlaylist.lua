1c7d0d97152dc85c73f5e32dfd0144964e967cc6
Niko Norwood <66198245+BrandtNorwood@users.noreply.github.com>
2024-05-03 16:07:44 -0500

    Dynamic Playlist Length
    No longer uses a fixed length and instead will loop till the URL is invalid, then reset back to 1

--- VaultPlaylist.lua
+++ VaultPlaylist.lua
@@ -2,17 +2,25 @@ local speakers = {peripheral.find("speaker")}
 
 if (#speakers > 0) then
     local i = 1
-    local playlistLength = 6
 
     while true do
-        shell.run("speaker play https://github.com/BrandtNorwood/CCRadio/raw/main/VaultPlaylist/Song"..i..".dfpwm")
+        local url = "https://github.com/BrandtNorwood/CCRadio/raw/main/VaultPlaylist/Song"..i..".dfpwm"
+        local exists = http.get(url)
+        
+        if exists then
+            exists.close()
+            shell.run("speaker play " .. url)
+            
+            i = i + 1
+            print("Playing next song.. ")
+        else
+            i = 1
+            print("Hit end of Playlist. Restarting Playlist..")
+        end
+        
         speakers[1].stop()
         os.sleep(2)
-        i = i + 1
-        if (i > playlistLength) then
-            i = 1
-        end
     end
 else
-    write ("Attach a speaker with 'attach <side> speaker' first")
+    print ("Attach a speaker with 'attach <side> speaker' first")
 end
