7ad8542364a282b914e1e915e4fe8cb6d4d2b690
Niko Norwood <66198245+BrandtNorwood@users.noreply.github.com>
2024-12-06 18:18:27 -0600

    Rendered Obsolete

--- player.lua
+++
@@ -1,8 +0,0 @@
-local speakers = {peripheral.find("speaker")}
-if multishell.getCount() > 10 then
-    os.reboot()
-end
-shell.run("speaker play "..(arg[1]).."/getCCFile")
-speakers[1].stop()
-os.sleep(2)
-shell.exit()
