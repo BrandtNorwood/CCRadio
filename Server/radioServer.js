9d2552db5eb0a251ce50c17170b227e864c26e5f
Niko Norwood <66198245+BrandtNorwood@users.noreply.github.com>
2024-12-06 18:17:33 -0600

    Implemented shuffle and fixed grace period bug

--- Server/radioServer.js
+++ Server/radioServer.js
@@ -1,6 +1,6 @@
-/*  Schedule Server V0.1
+/*  CC Radio Server V0.4
 
-    NodeJs script to function as a server for web based scheduling application
+    NodeJs script to function as a server computercraft ccradio clients
 
     Dependancies
         * NodeJS
@@ -18,6 +18,8 @@ const path = require('path');
 const { start } = require('repl');
 
 const port = 3050;
+
+const shuffle = true;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -39,60 +41,95 @@ var syncFrameStart = Date.now();
 var syncFrameLength = 0.0;
 
 //Loads song list using bygone's cached file feature (overkill but simple)
-songLibrary = new bygone.cachedFile("Song_Library/SongList.json")
+var songLibraryFile = new bygone.cachedFile("Song_Library/SongList.json")
+var songLibrary = {};
+
+
+
+//condensed into a function
+function updateSongLibrary(){
+    songLibraryFile.updateCache().then(()=>{
+        songLibrary = songLibraryFile.cache
+        if(shuffle){
+            shuffleArray(songLibrary);
+        }
+    });
+}
+updateSongLibrary();
+
+
+
 //keeps track of current song being served.
 var songCounter = 0;
 
-//period between songs when clients can still start
+//period after song start when clients can still start playback
 var gracePeriod = 3000;
+
+
+
+//stole from geeksforgeeks to shuffle the playlist array
+function shuffleArray(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    bygone.debugOutput("Shuffled "+arr.length+" songs")
+}
 
 
 
 //Returns time remainging in the sync frame
 app.get("/getSync", (req, res) => {
-    let allowPlay = true;
+    let allowPlay = false;
 
     let remainingSyncFrame = (Date.now() - (syncFrameStart + syncFrameLength)) * -1;
 
-    if (remainingSyncFrame > gracePeriod && Date.now() > syncFrameStart) {allowPlay = false;}
+    //if the sync frame is not over then dont allow play but do allow if less then 3 seconds has passed since start of sync frame
+    if (remainingSyncFrame < 0 || Date.now() < syncFrameStart + gracePeriod) {allowPlay = true;}
 
     res.send({canPlay:allowPlay, timerValue: remainingSyncFrame});
 });
 
 
 
-//Returns current song file
+//Returns current song file details
 app.get("/getSongDetails", (req, res) => {
-    currentSong = songLibrary.cache[songCounter];
+    currentSong = songLibrary[songCounter];
     res.send({name:currentSong.name,artist:currentSong.artist,length:currentSong.length})
-
-
 });
 
 
 //Returns Computercraft coded file to client
 app.get("/getCCFile",(req, res) =>{
-    currentSong = songLibrary.cache[songCounter];
+    currentSong = songLibrary[songCounter];
 
     let remainingSyncFrame = (Date.now() - (syncFrameStart + syncFrameLength)) * -1;
 
+    //If this is the first time a file is being requested on this loop then 
     if (remainingSyncFrame < gracePeriod){
+        //define syncFrame start as now and offset it by the grace period for other clients to still be able to join
         syncFrameLength = currentSong.length;
         syncFrameStart = Date.now();
-        syncFrameStart += gracePeriod;
 
-        songLibrary.updateCache();
-
-        setTimeout(nextSong, gracePeriod);
+        //we use a timeout so that the song switch does not happen till after the grace period is over
+        setTimeout(nextSong, gracePeriod*2);
         function nextSong() {
-            songCounter++
+            songCounter++;
 
             //if we have hit the end of the list head back to 0
-            songLibraryPure = songLibrary.cache; //had and issue doing .cache.length
-            if (songCounter >= songLibraryPure.length){
+            if (songCounter >= songLibrary.length){
                 songCounter = 0;
+
+                //reload and reshuffle (if enabled) song library in case anything was added
+                updateSongLibrary();
             }
-            currentSong = songLibrary.cache[songCounter];
+
+            //if pre roll exists on next song queue that and mark current song as pre roll
+
+            //if comming off a preroll or none exists queue song itself
+
+            //set next song as current song
+            currentSong = songLibrary[songCounter];
             bygone.debugOutput("Queued Next Song "+currentSong.name);
         }
     }
@@ -108,8 +145,7 @@ app.get("/getCCFile",(req, res) =>{
 //start Server
 app.listen(port, () =>{bygone.output(`Server Online at port ${port}`)})
 
-
-
+// gonna be honest... no clue if this function is nessesary anymore ¯\_(ツ)_/¯
 // Close the logger when application exits
 process.on('exit', () => {
     logger.close();
