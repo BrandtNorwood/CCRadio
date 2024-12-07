d0fc624e5412089f845aace42bd2fd391878d4d5
Niko Norwood <66198245+BrandtNorwood@users.noreply.github.com>
2024-12-06 18:18:47 -0600

    Script can now run in single shell

--- ccradio.lua
+++ ccradio.lua
@@ -1,29 +1,49 @@
-local speakers = {peripheral.find("speaker")}
+--set this to a server running ccradio
 local url = "http://localhost:3050"
+
+local speakers = {peripheral.find("speaker")}
 
 term.clear()
 term.setCursorPos(1,1)
 print("Hold CTRL T to quit")
 
+--BUG Does not work as expexted
+if (speakers[1] == nil) then
+    print("No speakers found!")
+    shell.exit()
+end
+
 function requestSong()
+    --make request for a sync frame
     local syncRequest = http.get(url.."/getSync")
 
+    --make sure the request was valid
     if (syncRequest ~= nil) then
 
+        --split request from json into object
         syncStatus = textutils.unserialiseJSON(syncRequest.readAll())
 
         if syncStatus.canPlay then
 
+            --request song details
             local detailRequest = http.get(url.."/getSongDetails")
             songDetails = textutils.unserialiseJSON(detailRequest.readAll())
             term.clear()
             term.setCursorPos(1,1)
             print("Hold CTRL T to quit")
-            term.setCursorPos(1,4)
+            term.setCursorPos(1,3)
             print("Song : "..songDetails.name)
             print("  By : "..songDetails.artist)
-            shell.openTab("player.lua", url)
-            os.sleep(10)
+
+            --single shell version
+            term.setCursorPos(1,6)
+            shell.run("speaker play "..url.."/getCCFile")
+            os.sleep(1)
+            speakers[1].stop()
+
+            term.clear()
+            term.setCursorPos(1,1)
+            print("Hold CTRL T to quit")
 
         else
             term.setCursorPos(1,2)
@@ -36,6 +56,7 @@ function requestSong()
         print ("Invalid Server Connection")
     end
 end
+--little loop with a delay to avoid ddosing a host
 while true do
     requestSong()
     os.sleep(0.5)
